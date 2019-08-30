9ca6174cdad948e23e79999b9a199188d5670c0b
Cherepanov-A <54631002+Cherepanov-A@users.noreply.github.com>
2019-08-30 14:50:34 +0300

    Add files via upload

---
+++ Sorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestApplication
+{
+    public class Sorter
+    {
+        public static int[] IntSort(int[] intArr)
+        {
+            if (intArr.Length < 2)
+            {
+                throw new ArgumentException("Array length is under 2");
+            }
+            else
+            {
+                for (int i = 1; i < intArr.Length; i++)
+                {
+                    if (intArr[i] < intArr[i - 1])
+                    {
+                        int temp = intArr[i];
+                        intArr[i] = intArr[i - 1];
+                        intArr[i - 1] = temp;
+                    }
+                }
+            }
+            return intArr;
+        }
+    }
+}
